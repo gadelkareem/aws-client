f1121c804b2ea78fdf11fccf947e1cfeb21217e8
Waleed Gadelkareem <gadelkareem@gmail.com>
2015-12-02 01:13:24 +0100

    Add dynamic columns

--- src/main/java/com/gadelkareem/awsclient/application/Controller.java
+++ src/main/java/com/gadelkareem/awsclient/application/Controller.java
@@ -10,21 +10,22 @@ import com.amazonaws.services.ec2.AmazonEC2Client;
 import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
 import com.amazonaws.services.ec2.model.Instance;
 import com.amazonaws.services.ec2.model.Reservation;
+import com.amazonaws.services.ec2.model.Tag;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
 
 
 public class Controller {
 
-    private final ObservableList<InstanceView> rows = FXCollections.observableArrayList();
     public TableView tableView;
-    public TableColumn id;
-    public TableColumn name;
+
 
     //INITIALIZE
     @FXML
@@ -38,45 +39,70 @@ public class Controller {
         AmazonEC2 amazonEC2 = new AmazonEC2Client(awsCredentials);
         amazonEC2.setRegion(Region.getRegion(Regions.EU_WEST_1));
 
-        id.setCellValueFactory(new PropertyValueFactory<InstanceView, String>("id"));
-        name.setCellValueFactory(new PropertyValueFactory<InstanceView, String>("name"));
+
+        addColumn(0, "Name");
+        addColumn(1, "Group");
+        addColumn(2, "Instance ID");
+        addColumn(3, "Instance Type");
+        addColumn(4, "Instance State");
+        addColumn(5, "Public IP");
+        addColumn(6, "Private IP");
+
         reservations:
         {
             for (Reservation reservation :
                     amazonEC2.describeInstances(new DescribeInstancesRequest()).getReservations()) {
                 for (Instance instance : reservation.getInstances()) {
-                    rows.add(new InstanceView(instance.getInstanceId(), instance.getSecurityGroups().get(0).getGroupName()));
 
+                    String instanceName = instance.getInstanceId();
+                    for (Tag tag : instance.getTags()) {
+                        if (tag.getKey().equals("Name")) {
+                            instanceName = tag.getValue();
+                            break;
+                        }
+                    }
+                    String[] values = {
+                            instanceName,
+                            instance.getSecurityGroups().get(0).getGroupName(),
+                            instance.getInstanceId(),
+                            instance.getInstanceType(),
+                            instance.getState().getName(),
+                            instance.getPublicIpAddress(),
+                            instance.getPrivateIpAddress()
+                    };
+                    addRow(values);
 //                    break reservations;
                 }
             }
         }
 
-        tableView.setItems(FXCollections.observableList(rows));
     }
-    //END OF INITIALIZE
 
-    public class InstanceView {
-
-        private SimpleStringProperty id;
-        private SimpleStringProperty name;
-
-
-        public InstanceView(String s1, String s2) {
-            id = new SimpleStringProperty(s1);
-            name = new SimpleStringProperty(s2);
+    private void addRow(String[] values) {
+        ObservableList<StringProperty> row = FXCollections.observableArrayList();
+        for (String value : values) {
+            row.add(new SimpleStringProperty(value));
         }
+        tableView.getItems().add(row);
+    }
 
-        public String getName() {
-            return name.get();
-        }
+    private void addColumn(final int columnIndex, String columnTitle) {
+        
+        TableColumn<ObservableList<StringProperty>, String> column = new TableColumn<ObservableList<StringProperty>, String>();
+        column.setText(columnTitle);
 
-
-        public String getId() {
-            return id.get();
-        }
-
-
+        column.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ObservableList<StringProperty>, String>, ObservableValue<String>>() {
+            public ObservableValue<String> call(
+                    TableColumn.CellDataFeatures<ObservableList<StringProperty>, String> cellDataFeatures) {
+                ObservableList<StringProperty> values = cellDataFeatures.getValue();
+                if (columnIndex >= values.size()) {
+                    return new SimpleStringProperty("");
+                } else {
+                    return cellDataFeatures.getValue().get(columnIndex);
+                }
+            }
+        });
+        tableView.getColumns().addAll(column);
     }
 
 
