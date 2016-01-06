2cbc8ec545cfe18f60f84203e48a0fa8dc14b8a8
Waleed Gadelkareem <gadelkareem@gmail.com>
2016-01-06 02:00:13 +0100

    Fix remove first column if no tag name

--- src/main/java/com/gadelkareem/awsclient/application/Controller.java
+++ src/main/java/com/gadelkareem/awsclient/application/Controller.java
@@ -197,7 +197,7 @@ public class Controller {
                         row.add(new SimpleStringProperty(""));
                         row.add(new SimpleStringProperty(instance.getInstanceId()));
                         if (userPreferences.getBoolean("view.column.load", false)) {
-                            String instanceLoad = Double.toString(monitorInstance(cloudWatchClient, instance.getInstanceId().toString()));
+                            String instanceLoad = Double.toString(monitorInstance(cloudWatchClient, instance.getInstanceId()));
                             row.add(new SimpleStringProperty(instanceLoad));
                         }
                         row.add(new SimpleStringProperty(instance.getSecurityGroups().get(0).getGroupName()));
@@ -214,7 +214,7 @@ public class Controller {
                             row.add(new SimpleStringProperty(""));
                         }
                         for (Tag tag : instance.getTags()) {
-                            if (tag.getKey().equals(firstColumnKey)) {
+                            if (tag.getKey().equals(firstColumnKey) && !tag.getValue().isEmpty()) {
                                 row.set(0, new SimpleStringProperty(tag.getValue()));
                                 hasFirstColumnKey = true;
                             } else {
