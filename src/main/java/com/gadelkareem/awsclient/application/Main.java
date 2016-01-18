0aa5bc7a79406ba75f258e06f1e781bfc5d6d733
Waleed Gadelkareem <gadelkareem@gmail.com>
2016-01-18 23:39:16 +0100

    Adding icons

--- src/main/java/com/gadelkareem/awsclient/application/Main.java
+++ src/main/java/com/gadelkareem/awsclient/application/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -20,6 +21,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/application.fxml"));
         primaryStage.setTitle("AWS Client");
         primaryStage.setScene(new Scene(root, primaryScreenBounds.getWidth() / 1.5, primaryScreenBounds.getHeight() / 1.5));
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/AwsClient.png")));
         primaryStage.show();
     }
 }
